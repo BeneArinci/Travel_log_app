66c1e8844844220e90c60763bd3fd2c9b63587d5
BeneArinci <bene@tray.io>
2021-05-30 00:54:01 +0100

    Database set up with Mongoose +  Dotenv install for env variables.

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const { notFound, errorHandler } = require('./middlewares')
 
+mongoose.connect(process.env.DATABASE_URL)
 const app = express();
 
 app.use(morgan('common'))
@@ -11,7 +14,7 @@ app.use(morgan('common'))
 app.use(helmet())
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: process.env.CORS_ORIGIN
 }))
 
 app.get('/', (req, res) => {
@@ -26,5 +29,5 @@ app.use(errorHandler);
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => {
-  console.log(`listening at http://locakhost:${port}`);
+    console.log(`listening at http://locakhost:${port}`);
 });
