36891864562245558f9f8feab84adcf0e9176bf8
BeneArinci <bene@tray.io>
2021-05-29 01:02:39 +0100

    Require-setup main node packages.

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,17 @@
-const express = require ('express');
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet')
+const cors = require('cors')
 
 const app = express();
+app.use(morgan('common'))
+app.use(helmet())
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => {
-    console.log('listening at http://locakhost:1337');
-})
+  console.log(`listening at http://locakhost:${port}`);
+});
