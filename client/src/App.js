a7bdbbe7d721d6c3dc70ad797bd70961ec6b16dd
BeneArinci <bene@tray.io>
2021-06-01 00:27:24 +0100

    Add popup with info when clicking on specific position marker.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import ReactMapGL, {Marker} from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 
 import { listLogEntries } from './API'
 
 const App = () => {
     const [logEntries, setLogEntries] = useState([]);
+
+    const [showPopup, setShowPopup] = useState({});
 
     const [viewport, setViewport] = useState({
         width: '100vw',
@@ -35,31 +37,65 @@ const App = () => {
             {
                 logEntries.map((entry) => {
                     return (
-                        <Marker 
-                        key = {entry._id}
-                          
-                        latitude={entry.latitude}
-                        longitude={entry.longitude} 
-                        offsetLeft={-24} 
-                        offsetTop={-12}>
-                             <svg 
-                                className='marker' 
-                                viewBox="0 0 24 24" 
-                                strokeWidth="1.5" 
-                                fill="none" 
-                                strokeLinecap="round" 
-                                strokeLinejoin="round" 
-                                style={{
-                                    width: `24px`,
-                                    height: `24px`,
-                                }}>
-                                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
-                            </svg>   
-                        </Marker>
+                        <>
+                            <Marker 
+                                key = {entry._id}
+                                latitude={entry.latitude}
+                                longitude={entry.longitude} 
+                                offsetLeft={-24} 
+                                offsetTop={-12}
+                                onClick = { () => setShowPopup({
+                                    ...showPopup,
+                                    [entry._id]: true,
+                                })}>
+                                    <svg 
+                                        className='marker' 
+                                        viewBox="0 0 24 24" 
+                                        strokeWidth="1.5" 
+                                        fill="none" 
+                                        strokeLinecap="round" 
+                                        strokeLinejoin="round" 
+                                        style={{
+                                            width: `24px`,
+                                            height: `24px`,
+                                        }}>
+                                            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
+                                    </svg>
+                            </Marker>
+                            {
+                                showPopup[entry._id] ? (
+                                    <Popup
+                                    className='popup'
+                                    latitude={entry.latitude}
+                                    longitude={entry.longitude} 
+                                    closeButton={true}
+                                    closeOnClick={false}
+                                    onClose={()=>{
+                                        setShowPopup({
+                                            ...showPopup,
+                                            [entry._id]: false
+                                        })
+                                    }}
+                                    offsetLeft={-12} 
+                                    offsetTop={12}
+                                    anchor="bottom" >
+                                    <div>
+                                        <h3>{entry.title}</h3>
+                                        <p>{entry.comments}</p>
+                                        <p>rating {entry.rating}</p>
+                                        <small>Visited on: {new Date(entry.visitDate).toLocaleDateString()} </small>
+                                    </div>
+                                </Popup> 
+                                ) : null
+                            }
+                           
+
+                        </>
+                       
                     )
                 })
             }
-            
+         
         </ReactMapGL>
       );
 }
