fbba2c9157fea85b8ddc1c0035dbe5d301038f3f
BeneArinci <bene@tray.io>
2021-05-31 23:46:31 +0100

    Logged entries from DB + showing mposition markers for each of them.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,66 @@
 import * as React from 'react';
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import { useState, useEffect } from 'react';
+import ReactMapGL, {Marker} from 'react-map-gl';
+
+import { listLogEntries } from './API'
 
 const App = () => {
+    const [logEntries, setLogEntries] = useState([]);
+
     const [viewport, setViewport] = useState({
-        width: 1200,
-        height: 600,
+        width: '100vw',
+        height: '100vh',
         latitude: 51.5387,
         longitude: 0.0166,
-        zoom: 8
-      });
-     
+        zoom: 6
+    });
+
+    const getLogs = async () =>{
+        const logEntries = await listLogEntries()
+        setLogEntries(logEntries)
+    }
     
-      return (
+    useEffect(()=> {
+        getLogs()
+    }, [])
+
+    return (
         <ReactMapGL
-          {...viewport}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          onViewportChange={nextViewport => setViewport(nextViewport)}
-        />
+            {...viewport}
+            mapStyle='mapbox://styles/benearinci/ckpco19f575pn18rxsgliqagr'
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            onViewportChange={nextViewport => setViewport(nextViewport)}
+            
+        >
+            {
+                logEntries.map((entry) => {
+                    return (
+                        <Marker 
+                        key = {entry._id}
+                          
+                        latitude={entry.latitude}
+                        longitude={entry.longitude} 
+                        offsetLeft={-24} 
+                        offsetTop={-12}>
+                             <svg 
+                                className='marker' 
+                                viewBox="0 0 24 24" 
+                                strokeWidth="1.5" 
+                                fill="none" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round" 
+                                style={{
+                                    width: `24px`,
+                                    height: `24px`,
+                                }}>
+                                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
+                            </svg>   
+                        </Marker>
+                    )
+                })
+            }
+            
+        </ReactMapGL>
       );
 }
 
